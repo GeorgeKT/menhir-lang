5fe32cdd5d3fe738036f24383078d031d73923e9
Joris Guisson <joris.guisson@gmail.com>
2017-02-18 11:22:40 +0100

    Switch cobrac to clap

--- cobrac/src/main.rs
+++ cobrac/src/main.rs
@@ -2,48 +2,20 @@ extern crate llvm_sys as llvm;
 extern crate libffi;
 extern crate libc;
 extern crate libcobra;
-extern crate docopt;
-extern crate rustc_serialize;
+#[macro_use]
+extern crate clap;
 
 mod llvmbackend;
 
 use std::fs::File;
 use std::path::{Path, PathBuf};
 use std::process::exit;
-//use codegen::{CodeGenOptions, codegen, link, llvm_init};
-use docopt::Docopt;
 use libcobra::parser::{ParserOptions, parse_file};
 use libcobra::typechecker::{type_check_module};
 use libcobra::bytecode::{compile_to_byte_code, optimize_module, ByteCodeModule, OptimizationLevel};
 use libcobra::compileerror::{CompileResult, CompileError};
 use llvmbackend::{CodeGenOptions, llvm_code_generation};
 
-
-static USAGE: &'static str =  "
-Usage: cobra [options] <input-file>
-       cobra --help
-
-options:
-  --help                                       Show this message.
-  -D <dump>, --dump=<dump>                     Dump internal compiler state for debug purposes. Argument can be all, ast, bytecode or ir.
-                                               A comma separated list of these values is also supported.
-  -O, --optimize                               Optimize the code.
-  -I <imports>, --imports=<imports>            Directory to look for imports, use a comma separated list for more then one.
-  -o <output-file>, --output=<output-file>     Name of binary to create (by default input-file without the extensions).
-  -t <binary-type>, --type=<binary-type>       Binary type, allowed values: bytecode or exe
-";
-
-
-#[derive(RustcDecodable, Debug)]
-struct Args
-{
-    flag_dump: Option<String>,
-    flag_optimize: Option<bool>,
-    arg_input_file: Option<String>,
-    flag_output: Option<String>,
-    flag_type: Option<String>,
-    flag_imports: Option<String>,
-}
 
 fn default_output_file(input_file: &str) -> String
 {
@@ -91,18 +63,26 @@ fn parse(parser_options: &ParserOptions, input_file: &str, dump_flags: &str, opt
 
 fn run() -> CompileResult<i32>
 {
-    let args: Args = Docopt::new(USAGE)
-        .and_then(|d| d.decode())
-        .unwrap_or_else(|e| e.exit());
+    let matches = clap_app!(cobrac =>
+        (version: "1.0")
+        (author: "Joris Guisson <joris.guisson@gmail.com>")
+        (about: "cobra language compiler")
+        (@arg DUMP: -d --dump +takes_value "Dump internal compiler state for debug purposes. Argument can be all, ast, bytecode or ir. A comma separated list of these values is also supported.")
+        (@arg INPUT_FILE: +required "File to compile")
+        (@arg OPTIMIZE: -O --optimize "Optimize the code")
+        (@arg BINARY_TYPE: -t --type +takes_value "Binary type, allowed values: bytecode or exe")
+        (@arg OUTPUT_FILE: -o --output +takes_value "Name of binary to create (by default input file without the extensions)")
+        (@arg IMPORTS: -I --imports +takes_value "Directory to look for imports, use a comma separated list for more then one.")
+    ).get_matches();
 
-    let input_file = args.arg_input_file.expect("Missing input file argument");
-    let optimize = args.flag_optimize.unwrap_or(false);
-    let output_file = args.flag_output.unwrap_or_else(|| default_output_file(&input_file));
-    let binary_type = args.flag_type.unwrap_or("exe".into());
-    let dump_flags = args.flag_dump.unwrap_or_default();
+    let input_file = matches.value_of("INPUT_FILE").expect("No input file given");
+    let optimize = matches.is_present("OPTIMIZE");
+    let output_file = matches.value_of("OUTPUT_FILE").map(|v| v.to_string()).unwrap_or_else(|| default_output_file(&input_file));
+    let binary_type = matches.value_of("BINARY_TYPE").unwrap_or("exe");
+    let dump_flags = matches.value_of("DUMP").unwrap_or("");
 
     let parser_options = ParserOptions{
-        import_dirs: args.flag_imports
+        import_dirs: matches.value_of("IMPORTS")
             .map(|dirs| dirs.split(',').map(PathBuf::from).collect())
             .unwrap_or_else(Vec::new),
     };
@@ -122,8 +102,8 @@ fn run() -> CompileResult<i32>
             let opts = CodeGenOptions{
                 dump_ir: dump_flags.contains("ir") || dump_flags.contains("all"),
                 build_dir: "build".into(),
-                program_name: output_file,
-                optimize: args.flag_optimize.unwrap_or(false),
+                program_name: output_file.into(),
+                optimize: optimize,
             };
 
             llvm_code_generation(&bc_mod, &opts)
