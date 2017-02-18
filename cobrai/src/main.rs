fe917c0ac367286bf2bb7577bf743d5b678c4bef
Joris Guisson <joris.guisson@gmail.com>
2017-02-18 10:52:50 +0100

    Swtich to clap for argument parsing in interpreter

--- cobrai/src/main.rs
+++ cobrai/src/main.rs
@@ -1,5 +1,5 @@
 extern crate shrust;
-extern crate docopt;
+extern crate clap;
 extern crate rustc_serialize;
 extern crate itertools;
 extern crate libcobra;
@@ -8,25 +8,15 @@ mod interpreter;
 mod debugger;
 mod value;
 mod valueref;
-
 #[cfg(test)]
 mod tests;
 
 use std::fs::File;
 use std::process::exit;
-use docopt::Docopt;
+use clap::{App, Arg};
 use libcobra::bytecode::{ByteCodeModule, START_CODE_FUNCTION};
 use interpreter::{ExecutionError, run_byte_code};
 use debugger::debug_byte_code;
-
-static USAGE: &'static str =  "
-Usage: cobrai [options] <bytecode-file>
-       cobrai --help
-
-options:
-  --help                                       Show this message.
-  -d --debug                                   Run the interpreter in debug mode.
-";
 
 
 #[derive(RustcDecodable, Debug)]
@@ -38,12 +28,22 @@ struct Args
 
 fn run() -> Result<i32, ExecutionError>
 {
-    let args: Args = Docopt::new(USAGE)
-        .and_then(|d| d.decode())
-        .unwrap_or_else(|e| e.exit());
+    let matches = App::new("cobrai")
+        .version("0.1")
+        .author("Joris Guisson <joris.guisson@gmail.com>")
+        .about("cobra language interpreter")
+        .arg(Arg::with_name("debug")
+            .short("d")
+            .long("debug")
+            .help("Run the interpreter in debug mode"))
+        .arg(Arg::with_name("bytecode-file")
+            .help("Bytecode file to run")
+            .required(true)
+            .index(1))
+        .get_matches();
 
-    let bytecode_file = args.arg_bytecode_file.expect("Missing input file argument");
-    let run_debugger = args.flag_debug.unwrap_or(false);
+    let bytecode_file = matches.value_of("bytecode-file").expect("Missing input file argument");
+    let run_debugger = matches.is_present("debug");
 
     let mut file = File::open(&bytecode_file)
         .map_err(|err| ExecutionError(format!("Cannot open {}: {}", bytecode_file, err)))?;
