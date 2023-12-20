4c332db57a2ace48f9ff86027b808e0bb4adc223
Joris Guisson <joris.guisson@gmail.com>
2023-12-20 17:25:02 +0100

    Add scope.rs

---
+++ src/lazycode/scope.rs
@@ -0,0 +1,197 @@
+use crate::{
+    ast::{prefix, Expression, FunctionSignature, TreeFormatter, Type, RVO_RETURN_ARG},
+    target::Target,
+};
+
+use super::{compiler::expr_to_bc, instruction::Label, ByteCodeModule, Instruction, Operand};
+
+/*
+#[derive(Debug)]
+pub struct ScopePath {
+    path: Vec<(BasicBlockRef, usize)>,
+}
+
+impl ScopePath {
+    fn parent(&self) -> Option<ScopePath> {
+        if self.path.len() <= 1 {
+            None
+        } else {
+            let parent_len = self.path.len() - 1;
+            Some(ScopePath{
+                path: self.path.iter().take(parent_len).cloned().collect(),
+            })
+        }
+    }
+
+    fn p
+}
+
+*/
+
+#[derive(Debug)]
+pub enum ScopeNode {
+    Instruction(Instruction),
+    Scope(Scope),
+}
+
+#[derive(Debug)]
+pub struct Scope {
+    pub nodes: Vec<ScopeNode>,
+    unwind: Vec<Expression>,
+    parent_unwind: Vec<Expression>,
+    label_counter: usize,
+    sig: FunctionSignature,
+}
+
+impl Scope {
+    pub fn new(sig: FunctionSignature) -> Self {
+        Scope {
+            sig,
+            nodes: Vec::new(),
+            unwind: Vec::new(),
+            parent_unwind: Vec::new(),
+            label_counter: 1,
+        }
+    }
+    pub fn add_unwind_call(&mut self, e: Expression) {
+        self.unwind.push(e);
+    }
+
+    pub fn rvo_var(&self) -> Operand {
+        Operand::Var {
+            name: RVO_RETURN_ARG.into(),
+            typ: self.sig.return_type.clone(),
+        }
+    }
+
+    pub fn rvo(&self) -> bool {
+        self.sig.rvo
+    }
+
+    pub fn label(&mut self) -> Label {
+        let lbl = self.label_counter;
+        self.label_counter += 1;
+        Label::new(lbl)
+    }
+
+    pub fn start_label(&mut self, lbl: Label) {
+        self.nodes
+            .push(ScopeNode::Instruction(Instruction::Label { label: lbl }));
+    }
+
+    pub fn add(&mut self, inst: Instruction) {
+        if let Some(ScopeNode::Instruction(last)) = self.nodes.last() {
+            if last.is_terminator() && inst.is_terminator() {
+                panic!("Double terminator");
+                return;
+            }
+        }
+        if inst.is_void_store() {
+            // Ignore void stores
+            return;
+        }
+        self.nodes.push(ScopeNode::Instruction(inst));
+    }
+
+    pub fn alias(&mut self, name: &str, value: Operand, typ: Type) -> Operand {
+        let name: String = name.into(); //self.add_name(name);
+        let var = Operand::Var {
+            name: name.clone(),
+            typ: typ.clone(),
+        };
+
+        self.add(Instruction::Alias { name, value, typ });
+        var
+    }
+
+    pub fn declare(&mut self, name: &str, init: Option<Operand>, typ: Type) -> Operand {
+        let name: String = name.into(); //self.add_name(name);
+        let var = Operand::Var {
+            name: name.clone(),
+            typ: typ.clone(),
+        };
+
+        // Ignore void types
+        if typ != Type::Void {
+            self.add(Instruction::Declare { name, init, typ });
+        }
+        var
+    }
+
+    pub fn exit(&mut self, bc_mod: &mut ByteCodeModule, target: &Target, with: Instruction) {
+        let unwind = self.unwind.clone();
+        for e in unwind.iter().rev() {
+            let op = expr_to_bc(bc_mod, self, e, target);
+            self.add(Instruction::Exec { operand: op });
+        }
+        if let Instruction::Return { .. } = with {
+            let parent_unwind = self.parent_unwind.clone();
+            for e in parent_unwind.iter().rev() {
+                let op = expr_to_bc(bc_mod, self, e, target);
+                self.add(Instruction::Exec { operand: op });
+            }
+        }
+
+        self.add(with);
+    }
+
+    pub fn scope<F>(&mut self, f: F)
+    where
+        F: FnOnce(&mut Scope),
+    {
+        let mut scope = Scope::new(self.sig.clone());
+        scope.label_counter = self.label_counter;
+        scope.parent_unwind = self.parent_unwind.clone();
+        scope.parent_unwind.extend(self.unwind.iter().cloned());
+        f(&mut scope);
+        self.label_counter = scope.label_counter;
+        self.nodes.push(ScopeNode::Scope(scope));
+    }
+
+    pub fn visit_operands<Func: FnMut(&Operand)>(&self, f: &mut Func) {
+        for node in &self.nodes {
+            match node {
+                ScopeNode::Instruction(i) => i.visit_operands(f),
+                ScopeNode::Scope(s) => s.visit_operands(f),
+            }
+        }
+    }
+
+    pub fn for_each_instruction<Func: FnMut(&Instruction) -> bool>(&self, f: &mut Func) {
+        for node in &self.nodes {
+            match node {
+                ScopeNode::Instruction(i) => {
+                    if !f(i) {
+                        return;
+                    }
+                }
+                ScopeNode::Scope(s) => s.for_each_instruction(f),
+            }
+        }
+    }
+
+    pub fn last_instruction_is_return(&self) -> bool {
+        self.nodes
+            .last()
+            .map(|i| match i {
+                ScopeNode::Instruction(Instruction::Return { .. }) => true,
+                _ => false,
+            })
+            .unwrap_or(false)
+    }
+}
+
+impl TreeFormatter for Scope {
+    fn fmt(&self, level: usize, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        let pfx = prefix(level);
+        writeln!(f, "{pfx}scope:")?;
+        for node in &self.nodes {
+            match node {
+                ScopeNode::Instruction(i) => write!(f, "{pfx}{i}")?,
+                ScopeNode::Scope(s) => s.fmt(level + 1, f)?,
+            }
+        }
+
+        Ok(())
+    }
+}
